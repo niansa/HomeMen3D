0bce27fa3915fa7271591bbec4454f98e3b55ef0
Nils <nils@lyllia.fritz.box>
2021-06-07 17:40:10 +0200

    Major code style improvements

--- include/core/management/titleManagement.hpp
+++ include/core/management/titleManagement.hpp
@@ -1,28 +1,29 @@
 /*
-*   This file is part of HomeMen3D
-*   Copyright (C) 2019 DeadPhoenix8091, Epicpkmn11, Flame, RocketRobz, StackZ, TotallyNotGuy
-*
-*   This program is free software: you can redistribute it and/or modify
-*   it under the terms of the GNU General Public License as published by
-*   the Free Software Foundation, either version 3 of the License, or
-*   (at your option) any later version.
-*
-*   This program is distributed in the hope that it will be useful,
-*   but WITHOUT ANY WARRANTY; without even the implied warranty of
-*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*   GNU General Public License for more details.
-*
-*   You should have received a copy of the GNU General Public License
-*   along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*
-*   Additional Terms 7.b and 7.c of GPLv3 apply to this file:
-*       * Requiring preservation of specified reasonable legal notices or
-*         author attributions in that material or in the Appropriate Legal
-*         Notices displayed by works containing it.
-*       * Prohibiting misrepresentation of the origin of that material,
-*         or requiring that modified versions of such material be marked in
-*         reasonable ways as different from the original version.
-*/
+ *   This file is part of HomeMen3D
+ *   Copyright (C) 2019 DeadPhoenix8091, Epicpkmn11, Flame, RocketRobz, StackZ,
+ * TotallyNotGuy
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *   Additional Terms 7.b and 7.c of GPLv3 apply to this file:
+ *       * Requiring preservation of specified reasonable legal notices or
+ *         author attributions in that material or in the Appropriate Legal
+ *         Notices displayed by works containing it.
+ *       * Prohibiting misrepresentation of the origin of that material,
+ *         or requiring that modified versions of such material be marked in
+ *         reasonable ways as different from the original version.
+ */
 
 #ifndef TITLEMANAGEMENT_HPP
 #define TITLEMANAGEMENT_HPP
@@ -32,29 +33,36 @@
 #include <citro2d.h>
 #include <string>
 
-class TitleManagement
-{
+class TitleManagement {
 public:
-	TitleManagement(void) = default;
-	~TitleManagement(void);
+  TitleManagement(void) = default;
+  ~TitleManagement(void);
 
-	// Title Handling.
-	bool load(u64 id, FS_MediaType mediaType);
-	u32 highId(void);
-	u32 lowId(void);
-	u64 ID(void);
-	FS_MediaType mediaType(void);
-	std::string name(void);
-	std::string Author(void);
-	C2D_Image icon(void);
+  // Title Handling.
+  bool load(u64 id, FS_MediaType mediaType);
+
+  // Getters
+  u32 highId(void) { return (u32)(mId >> 32); }
+
+  u32 lowId(void) { return (u32)mId; }
+
+  u64 ID(void) { return (u64)highId() << 32 | lowId(); }
+
+  FS_MediaType mediaType(void) { return mMedia; }
+
+  C2D_Image icon(void) { return mIcon; }
+
+  std::string name(void) { return mName; }
+
+  std::string Author(void) { return mAuthor; }
 
 private:
-	u64 mId;
-	FS_MediaType mMedia;
-	FS_CardType mCard;
-	C2D_Image mIcon;
-	std::string mName;
-	std::string mAuthor;
+  u64 mId;
+  FS_MediaType mMedia;
+  FS_CardType mCard;
+  C2D_Image mIcon;
+  std::string mName;
+  std::string mAuthor;
 };
 
 #endif
